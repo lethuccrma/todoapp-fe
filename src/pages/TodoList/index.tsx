2ff6283cf42a40c666bc1dcb800e8ee59f479274
Thuc Le <lethuc.crma@gmail.com>
2022-07-19 16:27:25 +0700

    feat: modify add todo

--- src/pages/TodoList/index.tsx
+++ src/pages/TodoList/index.tsx
@@ -1,10 +1,12 @@
 import React, { useCallback, useState } from 'react';
-import { Button, Form, Input, Modal } from 'antd';
+import { Button, DatePicker, Empty, Form, Input, Modal, Pagination, Select, Switch, Tag } from 'antd';
+import type { CustomTagProps } from 'rc-select/lib/BaseSelect';
 import { useNavigate } from 'react-router-dom';
 import _ from 'lodash';
 import { CardStatus, ICardData } from '../../types/ICard';
 import useTodos from './hook';
 import TodoCard from '../../components/TodoCard';
+import moment from 'moment';
 
 const { Search } = Input;
 
@@ -29,11 +31,32 @@ const dummyData = (() => {
   );
 })();
 
+const options = [{ value: 'work', color: 'gold', label: 'Work' }, { value: 'study', color: 'lime', label: 'Study' }, { value: 'relax', color: 'green', label: 'Relax' }, { value: 'heath', color: 'cyan', label: 'Heath' }];
+const tagRender = (props: CustomTagProps & {color?: string}) => {
+  const { label, value, closable, onClose } = props;
+  const onPreventMouseDown = (event: React.MouseEvent<HTMLSpanElement>) => {
+    event.preventDefault();
+    event.stopPropagation();
+  };
+  return (
+    <Tag
+      color={options.find((v) => v.value === value)?.color}
+      onMouseDown={onPreventMouseDown}
+      closable={closable}
+      onClose={onClose}
+      style={{ marginRight: 3 }}
+    >
+      {label}
+    </Tag>
+  );
+};
+
 function TodoList() {
 
   const [modalVisible, setModalVisible] = useState(false);
   const [form] = Form.useForm();
   const [page, setPage] = useState(1);
+  const pageSize = 100;
 
   const handleAddCard = () => {
     form.submit();
@@ -41,17 +64,21 @@ function TodoList() {
 
   const handleAddCardSubmit = ({
     title,
-    body,
+    content,
+    categories,
+    dueDate,
   }: {
     title: string;
-    body: string;
+    content: string;
+    categories: any;
+    dueDate: any;
   }) => {
-    // TODO: add todo card
+    console.log(title, content, categories, dueDate);
     setModalVisible(false);
   };
 
   /* Fetching todo */
-  const {todos, loading, error, refetch, pagination} = useTodos({page, take: 100});
+  const {todos, loading, error, refetch, pagination} = useTodos({page, take: pageSize});
 
   /* Search */
   const search = (txt: string) => console.log(txt);
@@ -74,13 +101,17 @@ function TodoList() {
         loading={false}
         type="primary"
         ghost
-        onClick={() => setModalVisible(true)}
+        onClick={() => {
+          setModalVisible(true);
+          form.resetFields();
+        }}
       >
         Add more todo
       </Button>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 p-5">
+      {todos.length === 0 ? <Empty className='m-10' /> :<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 p-5">
         {loading ? dummyData.map((v) => <TodoCard key={v.id} {...v} loading={true} />) : todos.map((v) => <TodoCard key={v.id} {...v} loading={false} />)}
-      </div>
+      </div>}
+      <Pagination className='mt-10' current={page} defaultPageSize={pageSize} total={pagination.total} disabled={loading} responsive />
 
       <Modal
         title="New Card"
@@ -97,6 +128,12 @@ function TodoList() {
           autoComplete="off"
           form={form}
           onFinish={handleAddCardSubmit}
+          initialValues={{
+            title: '',
+            content: '',
+            dueDate: moment(),
+            categories: [],
+          }}
         >
           <Form.Item
             label="Title"
@@ -106,11 +143,26 @@ function TodoList() {
             <Input />
           </Form.Item>
           <Form.Item
-            label="Body"
-            name="body"
-            rules={[{ required: true, message: 'Please input the body!' }]}
+            label="Content"
+            name="content"
+            rules={[{ required: true, message: 'Please input the content!' }]}
           >
-            <Input />
+            <Input.TextArea rows={4} className="resize-none" />
+          </Form.Item>
+          <Form.Item label="Due date" name="dueDate">
+            <DatePicker />
+          </Form.Item>
+          {/* <Form.Item label="Done" name="isDone">
+            <Switch defaultChecked={false} />
+          </Form.Item> */}
+          <Form.Item label="Categories" name="categories">
+            <Select
+              mode="multiple"
+              showArrow
+              tagRender={tagRender}
+              style={{ width: '100%' }}
+              options={options}
+            />
           </Form.Item>
         </Form>
       </Modal>
